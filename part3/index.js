de402869dd6ebcc6a0cb4245000bd0eaa3ef9476
sheva64 <yungluv64@gmail.com>
2025-08-14 19:25:58 +0300

    Added POST functionality for notes

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
   {
     id: "1",
@@ -18,6 +20,12 @@ let notes = [
     important: true
   }
 ]
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+  console.log(note)
+  response.json(note)
+})
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
