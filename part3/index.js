2afab88bf3e539041e8f5188a11ce1c9a36c3394
sheva64 <yungluv64@gmail.com>
2025-08-21 16:51:41 +0300

    Added exercise 3.5.

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   { 
     "id": "1",
@@ -48,6 +50,23 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'name or number missing' })
+  }
+
+  const person = {
+    id: String(Math.floor(Math.random() * 1000000)), // випадковий id
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
 })
 
 const PORT = 3001
